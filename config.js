7536302b74c0599f90b4be9787e5905f847b15ec
bijoya <bijoya10101@gmail.com>
2022-02-28 18:47:53 +0530

    versionUpdateSDK44

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAlhxmFuVDRGJjvkFPSS5Q3PCyMiEFxJPw",
+    authDomain: "storytellin-72416.firebaseapp.com",
+    projectId: "storytellin-72416",
+    storageBucket: "storytellin-72416.appspot.com",
+    messagingSenderId: "993908358863",
+    appId: "1:993908358863:web:63708cad49a4b945ba41ba"
+  };
